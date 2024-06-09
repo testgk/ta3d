49c29dfe2876d0877c3c28cf8dbeff42c1af43eb
Gadi Klein <gadik@liveu.tv>
2024-06-09 10:50:55 +0300

    Initial Commit1

---
+++ main.py
@@ -0,0 +1,7 @@
+import math
+from direct.task import Task
+from direct.gui.DirectGui import DirectButton
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import GeoMipTerrain, Texture, Filename, PNMImage, Vec3, Point3
+
+
